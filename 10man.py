d3db4817c117e45394281c1cd7e6f69b15b8d154
Thomas Stearns <rtstearn@ucsc.edu>
2022-12-12 14:35:47 -0800

    added 10man non functioning

--- 10man.py
+++ 10man.py
@@ -1,10 +1,24 @@
 import PySimpleGUI as sg
 import pyautogui
 import time
+import 10manlibrary
+from PIL import Image
+import os
+
+folder = 
+imageList = [f for f in folder]
+
+for i in 
+col1=[[sg.Text('Player 1')],
+      [sg.Image(imOut)]]
+col2=[[sg.Text('Player 2')],
+      [sg.Image(imOut)]]
 
 layout = [
     [sg.Text("Number of players", size=(30, 1)), sg.InputText()],
     [sg.Text("Number of characters", size=(30, 1)), sg.InputText()],
+    [sg.Column(col1, size=sz)],
+    [sg.Column(col2, size=sz)],
     [sg.Button("Start")],
     [sg.Button("Close")]
 ]
@@ -13,11 +27,19 @@ margins = (10, 10)
 
 window = sg.Window("10 Man Iron Man", layout, margins)
 
+charList = 10manlibrary.list_of_characters
+
 # event loop
 while True:
     event, values = window.read()
     if event == "Close" or event == sg.WIN_CLOSED:
         break
     elif event == "Start":
-        print("deez")
+        sz = values[1]
+        for x in range(values[0]):
+            charArray[x] = 10manlibrary.pc(sz)
+        for x in range(values[0]):
+            for y in range(sz):
+                image = Image.open(charArray[x][y])
+                window["10 Man Iron Man"].update(
 window.close()
