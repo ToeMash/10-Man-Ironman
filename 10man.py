62ac2e7002d8492f29bac4669077ae993fbe9988
b00nedawgy <brendanrboone@gmail.com>
2022-12-12 22:36:58 -0800

    almost working 10man.py

--- 10man.py
+++ 10man.py
@@ -3,22 +3,17 @@ import time
 from lib_10man import char_lineup
 from PIL import Image
 import os
+import sys
 
+#theme
 sg.theme('DarkAmber')
 
-sz = 0
-
-col1=[[sg.Text('Player 1', background_color='black', size=sz)],
-      *[[sg.Image(imOut),] for i in range(sz)]]
-col2=[[sg.Text('Player 2', background_color="black", size=sz)],
-      [[sg.Image(imOut),] for i in range(sz)]]
+#player_num = 0      #number of players
+character_num = 0   #number of characters
 
 layout = [
-    [sg.Text("Number of players", size=(30, 1)), sg.InputText()],
+#    [sg.Text("Number of players", size=(30, 1)), sg.InputText()],
     [sg.Text("Number of characters", size=(30, 1)), sg.InputText()],
-    [sg.Column(col1, key='-IMAGEOUT-')],
-    [sg.Column(col2, key='-IMAGEOT-')],
-    [sg.Image(key="-IMG-")],
     [sg.Button("Start")],
     [sg.Button("Close")]
 ]
@@ -27,20 +22,78 @@ margins = (10, 10)
 
 window = sg.Window("10 Man Iron Man", layout, margins)
 
-charList = []
-
-# event loop
+#gather information
+#event loop
 while True:
     event, values = window.read()
     if event == "Close" or event == sg.WIN_CLOSED:
-        break
+        sys.exit("program exited")
     elif event == "Start":
-        sz = int(values[1])
-        for x in range(int(values[0])):
-            charList.append(char_lineup(sz))
-        for x in range(int(values[0])):
-            for y in range(sz):
-                imOut = "chars/" + charList[x][y] + ".png"
-                window["-IMG-"].update(imOut)
-                window.refresh()
+        #get the values
+        #player_num = int(values[0])
+        character_num = int(values[0])
+        if character_num <= 0 or character_num > 10:    #player_num <= 0
+            sys.exit("inputed values are invalid")
+        print("worked")
+        break
 window.close()
+
+#show produced characters
+
+col1=[
+    [sg.Text('Player 1', background_color='black', size=0)],
+    [sg.Image(key="-IMG-0")],
+    [sg.Image(key="-IMG-1")],
+    [sg.Image(key="-IMG-2")],
+    [sg.Image(key="-IMG-3")],
+    [sg.Image(key="-IMG-4")],
+    [sg.Image(key="-IMG-5")],
+    [sg.Image(key="-IMG-6")],
+    [sg.Image(key="-IMG-7")],
+    [sg.Image(key="-IMG-8")],
+    [sg.Image(key="-IMG-9")]
+]
+col2=[
+    [sg.Text('Player 2', background_color="black", size=0)],
+    [sg.Image(key="-IMG2-0")],
+    [sg.Image(key="-IMG2-1")],
+    [sg.Image(key="-IMG2-2")],
+    [sg.Image(key="-IMG2-3")],
+    [sg.Image(key="-IMG2-4")],
+    [sg.Image(key="-IMG2-5")],
+    [sg.Image(key="-IMG2-6")],
+    [sg.Image(key="-IMG2-7")],
+    [sg.Image(key="-IMG2-8")],
+    [sg.Image(key="-IMG2-9")]
+]
+
+layout2 = [
+    [sg.Column(col1, element_justification='c'),
+    sg.Column(col2, element_justification='c')],
+    [sg.Button("Close")]
+]
+
+window2 = sg.Window("The Ironman", layout2, margins)
+window2.read()
+
+charList = []
+
+#player1
+charList = char_lineup(character_num)
+for i in range(len(charList)):
+    imOut = "chars/" + charList[i] + ".png"
+    window2["-IMG-" + str(i)].update(imOut)
+    window2.refresh()
+
+#player2
+charList = char_lineup(character_num)
+for i in range(len(charList)):
+    imOut = "chars/" + charList[i] + ".png"
+    window2["-IMG2-" + str(i)].update(imOut)
+    window2.refresh()
+
+while True:
+    event, values = window2.read()
+    if event == "Close" or event == sg.WIN_CLOSED:
+        break
+window2.close()
