441e14ddcccec34833fe6a425b7851865b701669
Thomas Stearns <rtstearn@ucsc.edu>
2022-12-12 21:26:59 -0800

    added some functions for the tierlist

--- lib_10man.py
+++ lib_10man.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 #list of ssbu characters
 list_of_characters = ['Mario', 'Donkey Kong', 'Link', 'Samus', 'Dark Samus', 'Yoshi', 'Kirby', 'Fox', 'Pikachu', 'Luigi', 'Ness', 'Captain Falcon', 'Jigglypuff', 'Peach', 'Daisy', 'Bowser', 'Ice Climbers', 'Sheik', 'Zelda', 'Dr. Mario', 'Pichu', 'Falco', 'Marth', 'Lucina', 'Young Link', 'Ganondorf', 'Mewtwo', 'Roy', 'Chrom', 'Mr. Game & Watch', 'Meta Knight', 'Pit', 'Dark Pit', 'Zero Suit Samus', 'Wario', 'Snake', 'Ike', 'Pokemon Trainer', 'Diddy Kong', 'Lucas', 'Sonic', 'King Dedede', 'Olimar', 'Lucario', 'R.O.B.', 'Toon Link', 'Wolf', 'Villager', 'Mega Man', 'Wii Fit Trainer', 'Rosalina & Luma', 'Little Mac', 'Greninja', 'Mii Brawler', 'Mii Swordfighter', 'Mii Gunner', 'Palutena', 'Pac-man', 'Robin', 'Shulk', 'Bowser Jr.', 'Duck Hunt', 'Ryu', 'Ken', 'Cloud', 'Corrin', 'Bayonetta', 'Inkling', 'Ridley', 'Simon', 'Richter', 'King K. Rool', 'Isabelle', 'Incineroar', 'Piranha Plant', 'Joker', 'Hero', 'Banjo & Kazooie', 'Terry', 'Byleth', 'Min Min', 'Steve', 'Sephiroth', 'Pyra-Mythra', 'Kazuya', 'Sora']
@@ -20,3 +21,23 @@ def char_lineup(num_characters):
         lineup.append(name)
 
     return lineup
+
+def gen_personal(filename):
+    tierDict = {}
+    tiers = ['S', 'A', 'B', 'C', 'D', 'F']
+
+    f = open(filename, "r")
+    for x in f:
+        tierDict[tiers[int(x)]] = x.split('_')
+    return tierDict
+
+
+def gen_global(directory):
+    dictOut = {}
+    for f in os.listdir(directory):
+        dictOut[f.split('.')[0]] = gen_personal(f)
+    return dictOut
+
+def print_dict(dictionary):
+    print(dictionary)
+
