27b0302430f658f157345ae548989f284004ef5a
Brendan Boone <brboone@ucsc.edu>
2022-12-16 01:34:22 -0800

    cleaned up some comments

--- 10man.py
+++ 10man.py
@@ -8,12 +8,10 @@ import sys
 #theme
 sg.theme('DarkAmber')
 
-#player_num = 0      #number of players
 character_num = 0   #number of characters
 matched_random = True
 
 layout = [
-##    [sg.Text("Number of players", size=(30, 1)), sg.InputText()],
     [sg.Text("Number of characters", size=(30, 1)), sg.InputText()],
     [sg.Checkbox("Matched random", default=True)],
     [sg.Button("Start")],
@@ -34,8 +32,6 @@ while True:
     if event == "Close" or event == sg.WIN_CLOSED:
         sys.exit("program exited")
     elif event == "Start":
-        #get the values
-        ##player_num = int(values[0])
         character_num = int(values[0])
         if values[1]:
             matched_random = True
@@ -43,7 +39,6 @@ while True:
             matched_random = False
         if character_num <= 0 or character_num > 10:    #player_num less than or equal to 0
             sys.exit("inputed values are invalid")
-        ##print("worked")
         break
 window.close()
 
@@ -59,7 +54,6 @@ def refresh_characters():
     for i in range(len(charList)):
         tier_color = tiers_color_dict[get_tier(player_one, charList[i], dic)]
         imOut = "chars/" + charList[i] + ".png"
-        #img = img_base64(imOut)
         window2["-IMG1-" + str(i)].update(image_filename = imOut)
         window2["-IMG1-" + str(i)].update(button_color = tier_color)
         window2["-IMG1-" + str(i)].ParentRowFrame.config(background = "grey")
@@ -75,7 +69,6 @@ def refresh_characters():
     for i in range(len(charList)):
         tier_color = tiers_color_dict[get_tier(player_two, charList[i], dic)]
         imOut = "chars/" + charList[i] + ".png"
-        #img = img_base64(imOut)
         window2["-IMG2-" + str(i)].update(image_filename = imOut),
         window2["-IMG2-" + str(i)].update(button_color = tier_color)
         window2["-IMG2-" + str(i)].ParentRowFrame.config(background = "grey")
@@ -116,14 +109,15 @@ layout2 = [
 ]
 
 #show produced characters
-
 window2 = sg.Window("The Ironman", layout2, margins, location = (400,0), element_justification = 'c', element_padding = 1)
 
+#list of usable buttons
 but_ton = []
 for i in range(1,3,1):
     for j in range(10):
         but_ton.append("-IMG"+str(i)+"-"+str(j))
 
+#window2 event loop
 while True:
     event, values = window2.read()
     print(event)
@@ -142,7 +136,6 @@ while True:
             print(saw)
         else:
             window2[event].update(button_color = "grey")
-            #window2[event].ParentRowFrame.config(background = "grey")
             window2["f"+event].Widget.config(background = "grey")
             saw.append(event)
             print(saw)
